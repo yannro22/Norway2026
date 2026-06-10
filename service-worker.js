208d5b00558d4a15b191b3566239383d30ff8bb0
yannro22 <rjpwr242vt@privaterelay.appleid.com>
2026-06-10 16:24:02 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'norway-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
